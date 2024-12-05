60f9d88457cc4dfbfa217010112f73753253d867
Ruben Laso <laso@par.tuwien.ac.at>
2024-12-05 16:22:23 +0100

    style: format example code

--- example/ncclbench_example.cpp
+++ example/ncclbench_example.cpp
@@ -8,8 +8,7 @@
 
 #include <mpi.h>
 
-struct Options
-{
+struct Options {
     std::string operation = "ncclAllReduce";
     std::string data_type = "float";
     std::vector<size_t> sizes_bytes;
@@ -21,8 +20,7 @@ struct Options
     bool csv = false;
 };
 
-auto main(int argc, char * argv[]) -> int
-{
+auto main(int argc, char *argv[]) -> int {
     MPI_Init(&argc, &argv);
 
     int rank;
@@ -32,22 +30,43 @@ auto main(int argc, char * argv[]) -> int
 
     Options options;
 
-    app.add_option("-o,--operation", options.operation, "NCCL operation. E.g. ncclAllReduce")->required();
-    app.add_option("-d,--data-type", options.data_type, "Data type: [byte, char, int, float, double]")->required();
-    app.add_option("-s,--sizes", options.sizes_bytes, "Size(s) in bytes. E.g.: 1024 2048 4096")->required()->check(CLI::NonNegativeNumber);
+    app.add_option("-o,--operation", options.operation,
+                   "NCCL operation. E.g. ncclAllReduce")
+        ->required();
+    app.add_option("-d,--data-type", options.data_type,
+                   "Data type: [byte, char, int, float, double]")
+        ->required();
+    app.add_option("-s,--sizes", options.sizes_bytes,
+                   "Size(s) in bytes. E.g.: 1024 2048 4096")
+        ->required()
+        ->check(CLI::NonNegativeNumber);
     app.add_flag("-b,--blocking", options.blocking, "Blocking or non-blocking");
     app.add_flag("--csv", options.csv, "Output in CSV format");
 
     // Only one of --iterations or --time can be used
-    auto grp_bench_its_secs = app.add_option_group("Bench. iterations or seconds");
-    grp_bench_its_secs->add_option("-i,--iterations", options.benchmark_its, "Number of benchmark iterations")->check(CLI::PositiveNumber);
-    grp_bench_its_secs->add_option("-t,--time", options.benchmark_secs, "Benchmark time in seconds")->check(CLI::PositiveNumber);
+    auto grp_bench_its_secs =
+        app.add_option_group("Bench. iterations or seconds");
+    grp_bench_its_secs
+        ->add_option("-i,--iterations", options.benchmark_its,
+                     "Number of benchmark iterations")
+        ->check(CLI::PositiveNumber);
+    grp_bench_its_secs
+        ->add_option("-t,--time", options.benchmark_secs,
+                     "Benchmark time in seconds")
+        ->check(CLI::PositiveNumber);
     grp_bench_its_secs->require_option(1);
 
     // Only one of --warmups or --warmup-time can be used
-    auto grp_warmup_its_secs = app.add_option_group("Warmup iterations or seconds");
-    grp_warmup_its_secs->add_option("-w,--warmups", options.warmup_its, "Number of warmup iterations")->check(CLI::NonNegativeNumber);
-    grp_warmup_its_secs->add_option("-W,--warmup-time", options.warmup_secs, "Warmup time in seconds")->check(CLI::NonNegativeNumber);
+    auto grp_warmup_its_secs =
+        app.add_option_group("Warmup iterations or seconds");
+    grp_warmup_its_secs
+        ->add_option("-w,--warmups", options.warmup_its,
+                     "Number of warmup iterations")
+        ->check(CLI::NonNegativeNumber);
+    grp_warmup_its_secs
+        ->add_option("-W,--warmup-time", options.warmup_secs,
+                     "Warmup time in seconds")
+        ->check(CLI::NonNegativeNumber);
     grp_warmup_its_secs->require_option(1);
 
     CLI11_PARSE(app, argc, argv);
@@ -56,13 +75,21 @@ auto main(int argc, char * argv[]) -> int
     config.operation = options.operation;
     config.data_type = options.data_type;
     config.blocking = options.blocking;
-    if (options.warmup_its == 0) { config.warmup_its_or_secs = options.warmup_secs; }
-    else { config.warmup_its_or_secs = options.warmup_its; }
-    if (options.benchmark_its == 0) { config.benchmark_its_or_secs = options.benchmark_secs; }
-    else { config.benchmark_its_or_secs = options.benchmark_its; }
+    if (options.warmup_its == 0) {
+        config.warmup_its_or_secs = options.warmup_secs;
+    } else {
+        config.warmup_its_or_secs = options.warmup_its;
+    }
+    if (options.benchmark_its == 0) {
+        config.benchmark_its_or_secs = options.benchmark_secs;
+    } else {
+        config.benchmark_its_or_secs = options.benchmark_its;
+    }
 
     if (rank == 0) {
-        std::cout << (options.csv ? ncclbench::Results::csv_header() : ncclbench::Results::header()) << std::endl;
+        std::cout << (options.csv ? ncclbench::Results::csv_header()
+                                  : ncclbench::Results::header())
+                  << std::endl;
     }
 
     for (const auto size : options.sizes_bytes) {
@@ -70,11 +97,11 @@ auto main(int argc, char * argv[]) -> int
         MPI_Barrier(MPI_COMM_WORLD);
         const auto res = ncclbench::run(config);
         if (rank == 0) {
-        	std::cout << (options.csv ? res.csv() : res.text()) << std::endl;
+            std::cout << (options.csv ? res.csv() : res.text()) << std::endl;
         }
     }
 
     MPI_Finalize();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
